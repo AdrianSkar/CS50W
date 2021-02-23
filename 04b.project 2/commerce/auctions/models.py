9187876ef83b3137d129de18baa4c95b29a98bb2
AdrianSkar <adrian@adrianskar.com>
2021-02-23 18:49:12 +0100

    add[p2]:basic create

--- 04b.project 2/commerce/auctions/models.py
+++ 04b.project 2/commerce/auctions/models.py
@@ -3,4 +3,29 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+	pass
+
+
+class Category(models.Model):
+	catName = models.CharField(max_length=64)
+
+
+class Bid(models.Model):
+	pass
+
+
+class Comment(models.Model):
+	pass
+
+
+class Listing(models.Model):
+	title = models.CharField(max_length=64)
+	desc = models.CharField(max_length=300)
+	# start_bid = models.IntegerField(default="0")
+	# curr_bid = models.IntegerField(blank=True)
+	# image_url = models.CharField(max_length=200)
+	# category = models.ForeignKey(
+	# 	Category, on_delete=models.CASCADE, related_name='category')
+
+	def __str__(self):
+		return f"{self.title}, {self.desc}"
--- 04b.project 2/commerce/auctions/views.py
+++ 04b.project 2/commerce/auctions/views.py
@@ -4,60 +4,80 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import *
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+	return render(request, "auctions/index.html")
 
 
 def login_view(request):
-    if request.method == "POST":
+	if request.method == "POST":
 
-        # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
+		# Attempt to sign user in
+		username = request.POST["username"]
+		password = request.POST["password"]
+		user = authenticate(request, username=username, password=password)
 
-        # Check if authentication successful
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            return render(request, "auctions/login.html", {
-                "message": "Invalid username and/or password."
-            })
-    else:
-        return render(request, "auctions/login.html")
+		# Check if authentication successful
+		if user is not None:
+			login(request, user)
+			return HttpResponseRedirect(reverse("index"))
+		else:
+			return render(request, "auctions/login.html", {
+				"message": "Invalid username and/or password."
+			})
+	else:
+		return render(request, "auctions/login.html")
 
 
 def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect(reverse("index"))
+	logout(request)
+	return HttpResponseRedirect(reverse("index"))
 
 
 def register(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
+	if request.method == "POST":
+		username = request.POST["username"]
+		email = request.POST["email"]
 
-        # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
-        if password != confirmation:
-            return render(request, "auctions/register.html", {
-                "message": "Passwords must match."
-            })
+		# Ensure password matches confirmation
+		password = request.POST["password"]
+		confirmation = request.POST["confirmation"]
+		if password != confirmation:
+			return render(request, "auctions/register.html", {
+				"message": "Passwords must match."
+			})
 
-        # Attempt to create new user
-        try:
-            user = User.objects.create_user(username, email, password)
-            user.save()
-        except IntegrityError:
-            return render(request, "auctions/register.html", {
-                "message": "Username already taken."
-            })
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
-    else:
-        return render(request, "auctions/register.html")
+		# Attempt to create new user
+		try:
+			user = User.objects.create_user(username, email, password)
+			user.save()
+		except IntegrityError:
+			return render(request, "auctions/register.html", {
+				"message": "Username already taken."
+			})
+		login(request, user)
+		return HttpResponseRedirect(reverse("index"))
+	else:
+		return render(request, "auctions/register.html")
+
+
+def create_listing(request):
+	if request.method == 'POST':
+		pass
+		title = request.POST["title"]
+		description = request.POST["description"]
+		# start_bid = request.POST["start_bid"]
+		# image_url = request.POST["image_url"]
+
+		# Create listing
+		try:
+			listing = Listing(title=title, desc=description)
+			listing.save()
+		except IntegrityError:
+			return render(request, "auctions/register.html", {
+				"message": "Username already taken."
+			})
+
+	return render(request, "auctions/create.html")
