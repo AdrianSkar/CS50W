6a298ee7247f30e8101e765ad8ee31e27a2b937f
AdrianSkar <adrian@adrianskar.com>
2021-03-11 18:49:22 +0100

    add[p2]:allow /listings as index page too

--- 04b.project 2/commerce/auctions/urls.py
+++ 04b.project 2/commerce/auctions/urls.py
@@ -1,14 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
-	path("", views.index, name="index"),
+	path("listings/<int:listing_id>", views.listing_view, name="listing"),
 	path("login", views.login_view, name="login"),
 	path("logout", views.logout_view, name="logout"),
 	path("register", views.register_view, name="register"),
 	path("create", views.create_listing_view, name="create"),
 	path("watchlist", views.watchlist_view, name="watchlist"),
-	path("listings/<int:listing_id>", views.listing_view, name="listing"),
+	re_path(r'^$|listings', views.index, name="index"),
 ]
 
