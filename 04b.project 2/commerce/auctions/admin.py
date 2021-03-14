8d4df781eeb5e8776e90513149319e2370b2da70
AdrianSkar <adrian@adrianskar.com>
2021-03-14 12:57:21 +0100

    refactor[p2]:admin comment fields

--- 04b.project 2/commerce/auctions/admin.py
+++ 04b.project 2/commerce/auctions/admin.py
@@ -3,15 +3,17 @@ from .models import *
 
 
 class ListingAdmin(admin.ModelAdmin):
-		list_display = ('title', 'desc', 'price', 'image_url', 'category', 'lister')
+		list_display = ('status', 'title', 'desc', 'price', 'image_url', 'category', 'lister')
 
 class ListingBid(admin.ModelAdmin):
 	list_display= ('amount', 'bidder', 'listing')
 
+class ListingComment(admin.ModelAdmin):
+	list_display = ('poster', 'listing')
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Bid, ListingBid)
-admin.site.register(Comment)
+admin.site.register(Comment, ListingComment)
 admin.site.register(Listing, ListingAdmin)
